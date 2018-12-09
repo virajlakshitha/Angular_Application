20ba4567867d605d63e315f0aebb07588892c066
viraj lakshitha <virajlakshitha39@gmail.com>
2018-12-09 23:44:35 +0530

    A search button added to the home interface

---
+++ src/app/template/template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
